28e8435d589887f5c7cc220a99d6d9921406ddad
Benjamin LANET <benjamin.lanet@besse-signature.com>
2015-10-12 17:40:12 +0200

    Start Tests

--- src/BbLigueBundle/Tests/Controller/DefaultControllerTest.php
+++ src/BbLigueBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,16 +3,73 @@
 namespace BbLigueBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class DefaultControllerTest extends WebTestCase
 {
+    private $client = null;
+
+    public function setUp()
+    {
+        $this->client = static::createClient();
+    }
     public function testIndex()
+    {
+        $this->client->request('GET', '/');
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+        $this->client->request('GET', '/ligue/');
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+        $this->client->request('GET', '/coach/');
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+        $this->client->request('GET', '/admin/');
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+    }
+    public function testUser() {
+        $this->logInAsUser();
+        $this->client->request('GET', '/');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+    public function testAdmin() {
+        $this->logInAsAdmin();
+
+        $crawler = $this->client->request('GET', '/');
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+        $this->assertGreaterThan(0, $crawler->filter('#navigation:contains("Espace admin")')->count());
+    }
+    public function testService()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $this->client->request('GET', '/login');
+        $container = $this->client->getContainer();
+        $rules = $container->get('bb.rules');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+    }
+
+    private function logInAsUser()
+    {
+        $session = $this->client->getContainer()->get('session');
+
+        $firewall = 'main';
+        $token = new UsernamePasswordToken('user', null, $firewall, array('ROLE_USER'));
+        $session->set('_security_'.$firewall, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
+    }
+    private function logInAsAdmin()
+    {
+        $session = $this->client->getContainer()->get('session');
+
+        $firewall = 'main';
+        $token = new UsernamePasswordToken('admin', null, $firewall, array('ROLE_ADMIN'));
+        $session->set('_security_'.$firewall, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
     }
 }
