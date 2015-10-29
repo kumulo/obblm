7eb4388e134ef41fad4e474b2ee63760dedf5cb6
kumulo <lanet.benjamin@gmail.com>
2015-10-29 18:33:23 +0100

    Begin of Team sheet

--- src/BbLigueBundle/Entity/PlayerByJourney.php
+++ src/BbLigueBundle/Entity/PlayerByJourney.php
@@ -12,5 +12,43 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class PlayerByJourney extends BasePlayerByJourney
 {
+    /**
+     * Get Spp
+     *
+     * @return integer
+     */
+    public function getSpp()
+    {
+        return (
+            $this->completions * 1 +
+            $this->touchdowns * 3 +
+            $this->casualties * 2 +
+            $this->interceptions * 2 +
+            $this->mvps * 5
+        );
+    }
 
+    /**
+     * Set value
+     *
+     * @param integer $value
+     *
+     * @return PlayerByJourney
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return integer
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
 }
