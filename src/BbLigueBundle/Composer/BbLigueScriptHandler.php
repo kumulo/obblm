857fbc12decbf0cf9c7d0c4962747a6f0b9241c4
kumulo <lanet.benjamin@gmail.com>
2015-11-10 17:36:23 +0100

    Add media folders at composer install or update

---
+++ src/BbLigueBundle/Composer/BbLigueScriptHandler.php
@@ -0,0 +1,23 @@
+<?php
+namespace BbLigueBundle\Composer;
+
+use Composer\Script\CommandEvent;
+use Sensio\Bundle\DistributionBundle\Composer\ScriptHandler;
+
+class BbLigueScriptHandler extends ScriptHandler
+{
+    /**
+     * @param $event CommandEvent A instance
+     */
+    public static function initBundle(CommandEvent $event)
+    {
+        $options = parent::getOptions($event);
+        $appDir = $options['symfony-app-dir'];
+
+        parent::executeCommand(
+            $event,
+            $appDir,
+            'bbLigue:media:init'
+        );
+    }
+}
